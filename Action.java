78b76a9ca237a31044769f5592edea828d98cc48
Phuong-Vy <159920161+Phuong-Vy@users.noreply.github.com>
2024-12-10 18:08:35 +0100

    Add files via upload

---
+++ Action.java
@@ -0,0 +1,14 @@
+
+/**
+ * Décrivez votre interface Action ici.
+ *
+ * @author  (votre nom)
+ * @version (un numéro de version ou une date)
+ */
+
+public interface Action
+{
+    void process();
+    
+    String toString();
+}
