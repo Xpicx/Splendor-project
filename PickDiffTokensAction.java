ffad6ddc56b8a5d6daa6a8cb82065b71a8035f33
Phuong-Vy <159920161+Phuong-Vy@users.noreply.github.com>
2024-12-11 13:13:34 +0100

    Update PickDiffTokensAction.java

--- PickDiffTokensAction.java
+++ PickDiffTokensAction.java
@@ -10,26 +10,29 @@ public class PickDiffTokensAction
     private String type1;
     private String type2;
     private String type3;
-    private Player joueur;
-    private Board plateau;
+
 
     /**
      * Constructeur d'objets de classe PickDiffTokensAction
      */
-    public PickDiffTokensAction(Player joueur,Board plateau,String type1,String type2,String type3)
+    public PickDiffTokensAction(String type1,String type2,String type3)
     {
         this.type1=type1;
         this.type2=type2;
         this.type3=type3;
-        this.joueur=joueur;
-        this.plateau=plateau;
     }
 
-    public void process(){
+    public void process(Player joueur,Board plateau){
+        plateau.updateNbResource(type1,-1);
+        joueur.updateNbResource(type1,1);
+        plateau.updateNbResource(type2,-1);
+        joueur.updateNbResource(type2,1);
+        plateau.updateNbResource(type3,-1);
+        joueur.updateNbResource(type3,1);
         
     }
     
     public String toString(){
-        return "Vous avez pris"+type1+", "+type2+", "+type3+". ";
+        return "Vous avez pris: 1"+type1+", 1"+type2+", 1"+type3+". ";
     }
 }
