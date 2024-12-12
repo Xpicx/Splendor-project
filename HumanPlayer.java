5cc49049463de6b6d62fd0e2eac53b6959f0cae3
Pierrot <165144767+Robespierrot@users.noreply.github.com>
2024-12-12 12:14:54 +0100

    Update HumanPlayer.java

--- HumanPlayer.java
+++ HumanPlayer.java
@@ -1,38 +1,62 @@
+import java.util.Scanner;
+import java.util.ArrayList;
 
 public class HumanPlayer extends Player
 {
     public HumanPlayer(int id, String name){
         super(id,name);
     }
-    public Action chooseAction(){
-    System.out.println("Veuillez choisir une action: ");
+    
+    public Action chooseAction(Board board){
+        System.out.println("Veuillez choisir une action: ");
         System.out.println("Entrer 1 pour: Prendre deux jetons de la même ressource");
         System.out.println("Entrer 2 pour: Prendre trois jetons de ressources différentes");
         System.out.println("Entrer 3 pour: Acheter une carte développement");
+        System.out.println("Entrer 4 pour: Passer votre tour");
         Scanner scanner=new Scanner(System.in);
         int choix=scanner.nextInt(); //Lecture entrée clavier
+        Action result = null;
         
         //Choix que peut faire le joueur
-        while(choix!=1 || choix!=2 || choix!=3){
-            System.out.println("Choisir un chiffre entre 1 et 3");
+        while(choix!=1 && choix!=2 && choix!=3 && choix!=4){
+            System.out.println("Choisir un chiffre entre 1 et 4");
             choix=scanner.nextInt();
         }
         
         if(choix==1){
             System.out.println("Veuiller entrer un type de ressource");
             String choixRessource=scanner.nextLine();
-            while(choixRessource.equals("DIAMOND") || choixRessource.equals("SAPHIRE") || choixRessource.equals("EMERALD") || choixRessource.equals("RUBY") || choixRessource.equals("ONYX") ){
+            while(choixRessource.equals("DIAMOND") && choixRessource.equals("SAPHIRE") && choixRessource.equals("EMERALD") && choixRessource.equals("RUBY") && choixRessource.equals("ONYX") ){
                 System.out.println("Veuiller choisir un type de ressource");
                 choix=scanner.nextInt();
             }
-            PickSameTokensAction action= new PickSameTokensAction(player,board,choixRessource);
+            result = new PickSameTokensAction(choixRessource);
         }
+           
         if(choix==2){
-            PickDiffTokensAction action=new PickDiffTokensAction(player,board);
+            ArrayList<String> choixResources = new ArrayList<String>();
+            while (choixResources.size() < 4) { 
+                System.out.println("Veuiller entrer 3 types de ressource :");
+                String choixRessource=scanner.nextLine();
+                while(choixRessource.equals("DIAMOND") && choixRessource.equals("SAPHIRE") && choixRessource.equals("EMERALD") && choixRessource.equals("RUBY") && choixRessource.equals("ONYX") ){
+                    System.out.println("Veuiller choisir 3 types de ressource");
+                    choixResources.add(choixRessource);
+                }
+            }
+            result = new PickDiffTokensAction(choixResources.get(0), choixResources.get(1),choixResources.get(2));
         }
+        
         if(choix==3){
-            BuyCardAction action=new BuyCardAction(player,board);
+            System.out.println("Veuiller choisir une carte à acheter sur le plateau :");
+            int positionX = scanner.nextInt(); 
+            int positionY = scanner.nextInt();
+            result = new BuyCardAction(board.getCard(positionX, positionY));
+            
         }
+        
+        if(choix==4) {
+            result = new PassAction();
+        }
+        return result;
     }
-    
 }
