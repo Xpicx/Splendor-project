6e8c447465049359d13df42a8aae8cd0376c2d21
Xpicx <eliott56.hello@gmail.com>
2024-12-12 13:02:36 +0100

    Update DumbRobotPlayer.java

--- DumbRobotPlayer.java
+++ DumbRobotPlayer.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.Random;
 public class DumbRobotPlayer extends Player
 {
     public DumbRobotPlayer(int id, String name){
@@ -7,7 +8,7 @@ public class DumbRobotPlayer extends Player
     
     public Action chooseAction(Board board){
         for(int i = 0; i < 3; i++){
-            for(int j = 0; i < 4; j++){
+            for(int j = 0; j < 4; j++){
                 if(canBuyCard(board.getCard(i, j))){
                     BuyCardAction bca = new BuyCardAction(board.getCard(i, j));
                     return bca;
@@ -59,5 +60,30 @@ public class DumbRobotPlayer extends Player
         }
         PassAction pa = new PassAction();
         return pa;        
+    }
+    
+    public Resources chooseDiscardingTokens(){
+        String type = "";
+        while(super.getNbTokens()> 10){
+            Random random = new Random();
+            int rdm = random.nextInt(5);
+            if(rdm==0 && super.getNbResource("DIAMOND") > 0){
+                type = "DIAMOND";
+            }
+            if(rdm==1 && super.getNbResource("SAPPHIRE") > 0){
+                type = "SAPPHIRE";
+            }
+            if(rdm==2 && super.getNbResource("EMERALD") > 0){
+                type = "EMERALD";
+            }
+            if(rdm==3 && super.getNbResource("RUBY") > 0){
+                type = "RUBY";
+            }
+            if(rdm==4 && super.getNbResource("ONYX") > 0){
+                type = "ONYX";
+            }
+            super.updateNbResource(type, -1);
+        }
+        return super.getAvailableResources();
     }
 }
