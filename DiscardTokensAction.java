94f0084d3d910eaa81eb3c37e75656a7b41e0404
Phuong-Vy <159920161+Phuong-Vy@users.noreply.github.com>
2024-12-12 11:19:14 +0100

    Update DiscardTokensAction.java

--- DiscardTokensAction.java
+++ DiscardTokensAction.java
@@ -6,13 +6,20 @@
  * @version (un numéro de version ou une date)
  */
 public class DiscardTokensAction implements Action
-{
-    public DiscardTokensAction(){
-        
+{   
+    public Resources discardRessources;
+    
+    public DiscardTokensAction(Resources discardRessources){
+        this.discardRessources=discardRessources;
     }
     
     public void process(Player joueur,Board plateau){
-        
+        int[] couts= discardRessources.getResources();
+        joueur.updateNbResource("DIAMOND",-couts[0]);
+        joueur.updateNbResource("SAPPHIRE",-couts[1]);
+        joueur.updateNbResource("EMERALD",-couts[2]);
+        joueur.updateNbResource("RUBY",-couts[3]);
+        joueur.updateNbResource("ONYX",-couts[4]);
     }
     
     public String toString(){
